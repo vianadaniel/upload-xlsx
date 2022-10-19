361f256f45cd7af3118f4680bd9779f79e7d6364
Daniel Viana de Almeida <58748927+vianadaniel@users.noreply.github.com>
2022-10-19 11:12:28 -0300

    feat: upload excel

---
+++ src/config/multer.ts
@@ -0,0 +1,32 @@
+import { Options, diskStorage } from 'multer'
+import { resolve } from 'path'
+import { randomBytes } from 'crypto'
+import * as path from 'path'
+
+export const multerConfig = {
+  dest: resolve(__dirname, '..', '..', 'uploads'),
+  storage: diskStorage({
+    destination: (request, file, callback) => {
+      callback(null, resolve(__dirname, '..', '..', 'uploads'))
+    },
+    filename: (request, file, callback) => {
+      randomBytes(16, (error, hash) => {
+        if (error) {
+          callback(error, file.filename)
+        }
+        const filename = `${hash.toString('HEX')}.xlsx`
+        callback(null, filename)
+      })
+    }
+  }),
+  limits: {
+    fileSize: 5 * 1024 * 1024 // 5MB
+  },
+  fileFilter: function (req, file, cb) {
+    if (path.extname(file.originalname).toLowerCase() !== '.xlsx') {
+      return cb(new Error('Only xlsx are allowed'))
+    }
+
+    cb(null, true)
+  }
+} as Options
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import * as express from "express";
+import routes from "./routes";
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+app.listen(3333, () => {
+    console.log(`Upload Service running on port 3333`);
+});
